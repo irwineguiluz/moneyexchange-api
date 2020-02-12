19c28820b6dd0683cf7dcbc69a614e1cc758581d
Irwin <ijea92@gmail.com>
2020-02-12 00:35:56 -0500

    to save external api response in cache server

--- app/Http/Controllers/CurrenciesFeedController.php
+++ app/Http/Controllers/CurrenciesFeedController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Validation\ValidationException;
 use Zttp\Zttp;
 
 class CurrenciesFeedController extends Controller
 {
     public function index()
     {
-        $rates_feed = Zttp::get(env('EXCHANGERATES_FEED_API') . 'latest?symbols=EUR&base=USD')->body();
+        if (Cache::has('rates')) {
+            $response = [
+                'rates' => Cache::get('rates'),
+                'from' => 'cache',
+            ];
+        } else {
+            $expires_at = Carbon::now()->addMinutes(1);
+            $rates_feed = Zttp::get(env('EXCHANGERATES_FEED_API') . 'latest?symbols=EUR&base=USD');
+            $response = [
+                'rates' => $rates_feed->body(),
+                'from' => 'exchangeratesapi.io',
+            ];
+
+            if ($rates_feed->isSuccess()) {
+                Cache::put('rates', $rates_feed->body(), $expires_at);
+            } else {
+                return response()->json([
+                    'error' => 'It has ocurred an error with the external API',
+                ], 424);
+            }
+        }
 
         return response()->json([
-            'ratesFeed' => $rates_feed,
-        ]);
+            'ratesFeed' => $response,
+        ], 200);
     }
 }
